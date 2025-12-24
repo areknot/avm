37b121a80a736a00c522c1784cf95e26d8ae5eb0
areknot <piaddiction@gmail.com>
2025-12-24 18:50:15 +0900

    Add runtime.c and runtime.h

---
+++ src/runtime.c
@@ -0,0 +1,48 @@
+#include "runtime.h"
+#include <stdlib.h>
+
+ZAM_value_t* pop(ZAM_stack_t** stp) {
+  if (stp == NULL || *stp == NULL) return NULL;
+
+  ZAM_stack_t* st = *stp;
+  ZAM_value_t* val = st->val;
+
+  *stp = st->next;
+  free(st);
+  return val;
+}
+
+_Bool push(ZAM_stack_t** stp, ZAM_value_t* val) {
+  ZAM_stack_t* node = NULL;
+  node = malloc(sizeof(ZAM_stack_t));
+
+  if (!node) return false;
+
+  node->val = val;
+  node->next = *stp;
+  *stp = node;
+  return true;
+}
+
+ZAM_env_t* extend(ZAM_env_t *env, ZAM_value_t *val) {
+  ZAM_env_t *node = NULL;
+  node = malloc(sizeof(ZAM_env_t));
+
+  if (!node) return NULL; // Returns NULL if malloc failed.
+
+  node->val = val;
+  node->next = env;
+  return node;
+}
+
+ZAM_value_t* lookup(ZAM_env_t *env, int index) {
+  int i = 0;
+
+  while (env != NULL) {
+    if (i == index) return env->val;
+    i++;
+    env = env->next;
+  }
+
+  return NULL;
+}
---
+++ src/runtime.h
@@ -0,0 +1,45 @@
+#include <stdbool.h>
+
+struct ZAM_code;
+typedef struct ZAM_code ZAM_code_t;
+
+typedef struct ZAM_value_node ZAM_value_t;
+typedef struct ZAM_stack_node ZAM_stack_t;
+typedef struct ZAM_env_node ZAM_env_t;
+
+// Value type
+typedef enum {
+    ZAM_IntVal,
+    ZAM_BoolVal,
+    ZAM_ClosVal,
+    ZAM_Epsilon
+} ZAM_value_kind;
+
+
+struct ZAM_value_node {
+  ZAM_value_kind kind;
+  int int_value;
+  _Bool bool_value;
+  ZAM_code_t* closure_code;
+  ZAM_env_t* closure_env;
+};
+
+
+struct ZAM_stack_node {
+  ZAM_value_t* val;
+  struct ZAM_stack_node* next;
+};
+
+ZAM_value_t* pop(ZAM_stack_t** stp);
+
+  _Bool push(ZAM_stack_t** stp, ZAM_value_t* val);
+
+
+struct ZAM_env_node {
+  ZAM_value_t* val;
+  struct ZAM_env_node* next;
+};
+
+ZAM_env_t* extend(ZAM_env_t *env, ZAM_value_t *val);
+
+ZAM_value_t* lookup(ZAM_env_t *env, int index);
