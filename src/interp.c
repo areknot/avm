de33c671f04ee5306f14b2977c50c30527ec6db2
areknot <piaddiction@gmail.com>
2026-01-04 20:02:11 +0900

    Add more runtime assertions in `interp.c`

--- src/interp.c
+++ src/interp.c
@@ -71,24 +71,45 @@ AVM_value_t *run() {
 
     switch (instr->kind) {
     case AVM_Ldi:
-      push(astack, new_int(instr->const_int));
+      if (!push(astack, new_int(instr->const_int)))
+        error("AVM_Ldi: Couldn't push %d.", instr->const_int);
       break;
 
     case AVM_Ldb:
-      push(astack, new_bool(instr->const_bool));
+      if (!push(astack, new_bool(instr->const_bool)))
+        error("AVM_Ldb: Couldn't push %s.", instr->const_bool ? "true" : "false");
       break;
 
-    case AVM_Access:
-      push(astack, lookup(env, instr->access));
+    case AVM_Access: {
+      AVM_value_t *val = lookup(env, instr->access);
+      if (val == NULL)
+        error("AVM_Access: Couldn't find the variable %d.", instr->access);
+
+      if (!push(astack, val))
+        error("AVM_Access: Couldn't push the found value.");
+
+      break;
+    }
+
+    case AVM_Closure: {
+      AVM_value_t *clos = new_clos(instr->addr, env);
+      if (clos == NULL)
+        error("AVM_Closure: Couldn't create a new closure.");
+
+      if (!push(astack, clos))
+        error("AVM_CLosure: Couldn't push the closure.");
       break;
 
-    case AVM_Closure:
-      push(astack, new_clos(instr->addr, env));
-      break;
+    }
 
-    case AVM_Let:
+
+    case AVM_Let: {
       env = extend(env, pop(astack));
+      if (env == NULL)
+        error("AVM_Let: Couldn't extend the environment.");
+
       break;
+    }
 
     case AVM_EndLet:
       env = env->next;
@@ -100,7 +121,9 @@ AVM_value_t *run() {
 
     case AVM_CJump: {
       AVM_value_t *val = pop(astack);
-      if (val->kind == AVM_BoolVal && !(val->bool_value)) {
+      if (val == NULL || val->kind != AVM_BoolVal) {
+        error("AVM_CJump: Expected a bool value.");
+      } else if (!val->bool_value) {
         pc = instr->addr;
       }
       break;
@@ -110,11 +133,12 @@ AVM_value_t *run() {
       AVM_value_t *val1 = pop(astack);
       AVM_value_t *val2 = pop(astack);
 
-      if (val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
+      if (val1 == NULL || val2 == NULL || val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
         error("AVM_Add: Expected two integer values.");
       }
 
-      push(astack, new_int(val1->int_value + val2->int_value));
+      if (!push(astack, new_int(val1->int_value + val2->int_value))) // x + y
+        error("AVM_Add: Couldn't push the result.");
       break;
     }
 
@@ -122,11 +146,12 @@ AVM_value_t *run() {
       AVM_value_t *val1 = pop(astack); // y
       AVM_value_t *val2 = pop(astack); // x
 
-      if (val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
+      if (val1 == NULL || val2 == NULL || val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
         error("AVM_Sub: Expected two integer values.");
       }
 
-      push(astack, new_int(val2->int_value - val1->int_value)); // x - y
+      if (!push(astack, new_int(val2->int_value - val1->int_value))) // x - y
+        error("AVM_Sub: Couldn't push the result.");
       break;
     }
 
@@ -134,11 +159,12 @@ AVM_value_t *run() {
       AVM_value_t *val1 = pop(astack); // y
       AVM_value_t *val2 = pop(astack); // x
 
-      if (val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
+      if (val1 == NULL || val2 == NULL || val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
         error("AVM_Le: Expected two integer values.");
       }
 
-      push(astack, new_bool(val2->int_value <= val1->int_value)); // x <= y
+      if (!push(astack, new_bool(val2->int_value <= val1->int_value))) // x <= y
+        error("AVM_Le: Couldn't push the result.");
       break;
     }
 
@@ -146,11 +172,12 @@ AVM_value_t *run() {
       AVM_value_t *val1 = pop(astack);
       AVM_value_t *val2 = pop(astack);
 
-      if (val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
+      if (val1 == NULL || val2 == NULL || val1->kind != AVM_IntVal || val2->kind != AVM_IntVal) {
         error("AVM_Eq: Expected two integer values.");
       }
 
-      push(astack, new_bool(val1->int_value == val2->int_value));
+      if (!push(astack, new_bool(val1->int_value == val2->int_value))) // x == y
+        error("AVM_Eq: Couldn't push the result.");
       break;
     }
 
@@ -159,14 +186,22 @@ AVM_value_t *run() {
       AVM_value_t *func = pop(astack);
       AVM_value_t *arg = pop(astack);
 
-      if (func->kind != AVM_ClosVal) {
+      if (func == NULL || arg == NULL || func->kind != AVM_ClosVal) {
         error("AVM_Apply: Expected function application.");
       }
 
       // Push the current address and the environment to rstack.
-      push(rstack, new_clos(pc, env));
+      if (!push(rstack, new_clos(pc, env)))
+        error("AVM_Apply: Couldn't push the return address");
+
       // Extend the environment.
-      env = extend(extend(func->env, func), arg);
+      AVM_env_t *tmp = extend(func->env, func);
+      if (tmp == NULL)
+        error("AVM_Apply: Couldn't extend the environment.");
+
+      env = extend(tmp, arg);
+      if (env == NULL)
+        error("AVM_Apply: Couldn't extend the environment.");
 
       // Jump to the given address.
       pc = func->addr;
@@ -178,12 +213,18 @@ AVM_value_t *run() {
       AVM_value_t *func = pop(astack);
       AVM_value_t *arg = pop(astack);
 
-      if (func->kind != AVM_ClosVal) {
+      if (func == NULL || arg == NULL || func->kind != AVM_ClosVal) {
         error("AVM_Apply: Expected function application.");
       }
 
       // Extend the environment.
-      env = extend(extend(func->env, func), arg);
+      AVM_env_t *tmp = extend(func->env, func);
+      if (tmp == NULL)
+        error("AVM_Apply: Couldn't extend the environment.");
+
+      env = extend(tmp, arg);
+      if (env == NULL)
+        error("AVM_Apply: Couldn't extend the environment.");
 
       // Jump to the given address.
       pc = func->addr;
@@ -191,26 +232,44 @@ AVM_value_t *run() {
     }
 
     case AVM_PushMark:
-      push(astack, &epsilon);
+      if (!push(astack, &epsilon))
+        error("AVM_PushMark: Couldn't push the mark.");
       break;
 
     case AVM_Grab: {
       // Pop an argument.
       AVM_value_t *arg = pop(astack);
 
-      if (arg->kind == AVM_Epsilon) {
+      if (arg == NULL) {
+        error("AVM_Grab: Expected an argument.");
+      } else if (arg->kind == AVM_Epsilon) {
         // Get the caller's address.
         AVM_value_t *ret_addr = pop(rstack);
+        if (ret_addr == NULL)
+          error("AVM_Grab: Couldn't get the caller's address.");
 
         // Push the current address to astack.
-        push(astack, new_clos(pc, env));
+        AVM_value_t *tmp = new_clos(pc, env);
+        if (tmp == NULL)
+          error("AVM_Grab: Couldn't create a new closure.");
+        if (!push(astack, tmp))
+          error("AVM_Grab: Couldn't push the current address.");
 
         // Jump back to the caller.
         pc = ret_addr->addr;
         env = ret_addr->env;
       } else {
         // Extend the current environment and continue.
-        env = extend(extend(env, new_clos(pc, env)), arg);
+        AVM_value_t *cur_addr = new_clos(pc, env);
+        if (cur_addr == NULL)
+          error("AVM_Grab: Couldn't create a new closure.");
+
+        AVM_env_t * tmp = extend(env, cur_addr);
+        if (tmp == NULL)
+          error("AVM_Grab: Couldn't extend the environment.");
+        env = extend(tmp, arg);
+        if (env == NULL)
+          error("AVM_Grab: Couldn't extend the environment.");
       }
       break;
     }
@@ -220,32 +279,53 @@ AVM_value_t *run() {
       AVM_value_t *arg1 = pop(astack);
       AVM_value_t *arg2 = pop(astack);
 
-      if (arg2->kind == AVM_Epsilon) {
+      if (arg1 == NULL || arg2 == NULL) {
+        error("AVM_Return: Expected two items on the argument stack.");
+      } else if (arg2->kind == AVM_Epsilon) {
         // Get the caller's address.
         AVM_value_t *ret_addr = pop(rstack);
+        if (ret_addr == NULL)
+          error("AVM_Return: Couldn't get the caller's address.");
 
         // Push the first argument to astack.
-        push(astack, arg1);
+        if (!push(astack, arg1))
+          error("AVM_Return: Couldn't push the result to the argument stack.");
 
         // Jump back to the caller.
         pc = ret_addr->addr;
         env = ret_addr->env;
+      } else if (arg1->kind != AVM_ClosVal) {
+        error("AVM_Return: Invalid return address.");
       } else {
         // Call the first argument with its environment extended.
-        env = extend(extend(arg1->env, arg1), arg2);
+        AVM_env_t *tmp = extend(arg1->env, arg1);
+        if (tmp == NULL)
+          error("AVM_Return: Couldn't extend the environment.");
+
+        env = extend(tmp, arg2);
+        if (env == NULL)
+          error("AVM_Grab: Couldn't extend the environment.");
         pc = arg1->addr;
       }
       break;
     }
 
-    case AVM_Halt:
-      return pop(astack);
+    case AVM_Halt: {
+      AVM_value_t *res =  pop(astack);
+      if (res == NULL)
+        error("AVM_Halt: Expected a result.");
+
+      return res;
+    }
+
     }
   }
 }
 
 AVM_value_t* new_int(int i) {
   AVM_value_t* node = malloc(sizeof(AVM_value_t));
+  if (node == NULL)
+    error("new_int: Failed to allocate memory.");
   node->kind = AVM_IntVal;
   node->int_value = i;
   return node;
@@ -253,6 +333,8 @@ AVM_value_t* new_int(int i) {
 
 AVM_value_t *new_bool(_Bool b) {
   AVM_value_t* node = malloc(sizeof(AVM_value_t));
+  if (node == NULL)
+    error("new_int: Failed to allocate memory.");
   node->kind = AVM_BoolVal;
   node->bool_value = b;
   return node;
@@ -260,6 +342,8 @@ AVM_value_t *new_bool(_Bool b) {
 
 AVM_value_t *new_clos(int l, AVM_env_t *env) {
   AVM_value_t* node = malloc(sizeof(AVM_value_t));
+  if (node == NULL)
+    error("new_int: Failed to allocate memory.");
   node->kind = AVM_ClosVal;
   node->addr = l;
   node->env = env;
